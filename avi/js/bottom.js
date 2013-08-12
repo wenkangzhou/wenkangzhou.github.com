d5b330277f83793e5bb15e7ef7db41dbc2031432
wenkangzhou <koudaix700@gmail.com>
2013-08-12 23:37:19 +0800

    styleSheet update

--- avi/js/bottom.js
+++ avi/js/bottom.js
@@ -1,5 +1,5 @@
 document.write("<div id=\"footer\">");
-document.write("<a href=\"javascript:void(0)\" onclick=\"myAndroidJs.goToMenuPage(1);\" class=\"on\"><span class=\"foot-item icon_home\"></span>首页</a>");
-document.write("<a href=\"javascript:void(0)\" onclick=\"myAndroidJs.goToMenuPage(2);\" ><span class=\"foot-item icon_shop\"></span>应用</a>");
-document.write("<a href=\"javascript:void(0)\" onclick=\"myAndroidJs.goToMenuPage(3);\" ><span class=\"foot-item icon_set\"></span>游戏</a>");
+document.write("<a href=\"javascript:void(0)\" onclick=\"myAndroidJs.goToMenuPage(1);\" class=\"on\"><span class=\"foot-item icon_home\"><img src=\"./img/home_web.png\"><p>首页</P></span></a>");
+document.write("<a href=\"javascript:void(0)\" onclick=\"myAndroidJs.goToMenuPage(2);\" ><span class=\"foot-item icon_shop\"><img src=\"./img/hot_web.png\"><p>应用</P></span></a>");
+document.write("<a href=\"javascript:void(0)\" onclick=\"myAndroidJs.goToMenuPage(3);\" ><span class=\"foot-item icon_set\"><img src=\"./img/more_web.png\"><p>游戏</P></span></a>");
 document.write("<div>");

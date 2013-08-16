8421bdfa6602f44d1f0811c16855822332cb3e32
wenkangzhou <koudaix700@gmail.com>
2013-08-16 20:54:45 +0800

    c

--- avi/js/bottom.js
+++
@@ -1,5 +0,0 @@
-document.write("<div id=\"footer\">");
-document.write("<a href=\"javascript:void(0)\" onclick=\"myAndroidJs.goToMenuPage(1);\" class=\"on\"><span class=\"foot-item icon_home\"><img src=\"./img/home_web.png\"><p>首页</P></span></a>");
-document.write("<a href=\"javascript:void(0)\" onclick=\"myAndroidJs.goToMenuPage(2);\" ><span class=\"foot-item icon_shop\"><img src=\"./img/hot_web.png\"><p>应用</P></span></a>");
-document.write("<a href=\"javascript:void(0)\" onclick=\"myAndroidJs.goToMenuPage(3);\" ><span class=\"foot-item icon_set\"><img src=\"./img/more_web.png\"><p>游戏</P></span></a>");
-document.write("<div>");
